7254d5167f39d5111e5871576b9f5db9fc72de68
IGardocki <ian.d.gardocki@gmail.com>
2023-04-28 22:28:14 -0500

    up to classes in w3 crs

--- src/__test__/ts-practice.spec.ts
+++ src/__test__/ts-practice.spec.ts
@@ -1,6 +1,7 @@
-import {myString, myNumber, myBoolean, myAnyType, myUndefined, myNull,
-myStringArr, myNumArr, myTuple, myPet, myCatEnum, myUnionType, catAge, theDetectiveAge} from '../ts-practice';
+// import {myString, myNumber, myBoolean, myAnyType, myUndefined, myNull,
+// myStringArr, myNumArr, myTuple, myPet, myCatEnum, myUnionType, catAge, theDetectiveAge} from '../ts-practice';
 
+import * as tsPractice from '../ts-practice'
 // LOOP 1: a basic test that will pass ////////////////////////////
 describe('Test should', ()=> {
     test('pass', () => {
@@ -21,19 +22,19 @@ describe('Test should', ()=> {
 // SIMPLE TYPE TESTS
 describe('myString should', () => {
     test('be a string', () => {
-        expect(typeof(myString)).toBe('string');
+        expect(typeof(tsPractice.myString)).toBe('string');
     });
 });
 
 describe('myNumber should', () => {
     test('be a number', () => {
-        expect(typeof(myNumber)).toBe('number');
+        expect(typeof(tsPractice.myNumber)).toBe('number');
     });
 });
 
 describe('myBoolean should', () => {
     test('be a boolean', () => {
-        expect(typeof(myBoolean)).toBe('boolean');
+        expect(typeof(tsPractice.myBoolean)).toBe('boolean');
     });
 });
 
@@ -42,25 +43,25 @@ describe('myAnyType should', () => {
     test('be of the correct type', () => {
         // pick a type of the variable you will define, and update
         // the code to reflect this!
-        expect(typeof(myAnyType)).toBe('string')
+        expect(typeof(tsPractice.myAnyType)).toBe('string')
     })
 })
 
 describe('myUndefined should', () => {
     test('be of undefined type', () => {
-        expect(typeof(myUndefined)).toBe('undefined');
+        expect(typeof(tsPractice.myUndefined)).toBe('undefined');
     });
 });
 
 describe('myNull should', () => {
     test('be of null type', () => {
-        expect(typeof(myNull)).toBe('object');
+        expect(typeof(tsPractice.myNull)).toBe('object');
     });
 });
 
 describe('myStringArr', () => {
     test('should not not contain non-string elements', () => {
-        const nonStringArray = myStringArr.filter((arrayItem) => typeof(arrayItem) !== 'string');
+        const nonStringArray = tsPractice.myStringArr.filter((arrayItem) => typeof(arrayItem) !== 'string');
         expect(nonStringArray.length).toBe(0);
     });
 });
@@ -68,7 +69,7 @@ describe('myStringArr', () => {
 // ARRAY TESTS
 describe('myNumArr', () => {
     test('should not contain any non-number elements', () => {
-        const nonNumberArray = myNumArr.filter((arrayItem) => typeof(arrayItem) !== 'number');
+        const nonNumberArray = tsPractice.myNumArr.filter((arrayItem) => typeof(arrayItem) !== 'number');
         expect(nonNumberArray.length).toBe(0);
     });
 });
@@ -76,53 +77,82 @@ describe('myNumArr', () => {
 // TUPLE TESTS
 describe('myTuple should', () => {
     test('have a number as the zeroth element', () => {
-        expect(typeof(myTuple[0])).toBe('number');
+        expect(typeof(tsPractice.myTuple[0])).toBe('number');
     });
     test('have a string as the first element', () => {
-        expect(typeof(myTuple[1])).toBe('string');
+        expect(typeof(tsPractice.myTuple[1])).toBe('string');
     });
     test('have a boolean as the second element', () => {
-        expect(typeof(myTuple[2])).toBe('boolean');
+        expect(typeof(tsPractice.myTuple[2])).toBe('boolean');
     });
 })
 
 // OBJECT TESTS
 describe('myPet should', () => {
     test('have a name' ,() => {
-        expect(typeof(myPet.name)).toBe('string')
+        expect(typeof(tsPractice.myPet.name)).toBe('string')
     });
     test('have a weight', () => {
-        expect(typeof(myPet.weight)).toBe('number');
+        expect(typeof(tsPractice.myPet.weight)).toBe('number');
     });
 });
 
 // ENUM TESTS
 describe('myCatEnum should', () => {
     test('return appropriate numbers for values', () => {
-        expect(myCatEnum.Romad).toBe(0);
-        expect(myCatEnum.Smoke).toBe(1);
-        expect(myCatEnum.Fire).toBe(2);
+        expect(tsPractice.myCatEnum.Romad).toBe(0);
+        expect(tsPractice.myCatEnum.Smoke).toBe(1);
+        expect(tsPractice.myCatEnum.Fire).toBe(2);
     })
 })
 
 // ALIASES AND INTERFACES TYPES
 describe('theDetectiveAge should', () => {
     test('be of type number', () => {
-        expect(typeof(theDetectiveAge)).toBe('number');
+        expect(typeof(tsPractice.theDetectiveAge)).toBe('number');
     })
 })
 
 // UNION TYPE TESTS
 describe('myUnionType', () => {
     test('be of type string or number', () => {
-        const typeCheck = typeof(myUnionType) === 'string' || typeof(myUnionType) === 'number';
+        const typeCheck = typeof(tsPractice.myUnionType) === 'string' || typeof(tsPractice.myUnionType) === 'number';
         expect(typeCheck).toBe(true);
     })
 })
 
 // FUNCTION TESTS
+describe('getCatNumber function should', () => {
+    test('return number of cats I own', () => {
+        expect(tsPractice.getCatNumber()).toBe(1);
+    });
+});
+
+describe('myVoidCatFunction should', () => {
+    test('return nothing', () => {
+        expect(tsPractice.myVoidCatFunction()).toBeUndefined();
+    })
+})
+
 describe('addCats function should', () => {
-    const res = addCats(1,1);
+    test('Add numbers to give total cat number', () => {
+        const res = tsPractice.addCats(1,1);
+        expect(res).toBe('I have 2 cats!');
+    });
+});
+
+
+// CLASS TESTS
+describe('catClass should', () => {
+    test('return the proper name', () => {
+        const cat = new tsPractice.catClass('Pickles');
+        expect(cat.getName()).toBe('Pickles');
+    });
+    test('return name and breed', () =>{
+        const cat = new tsPractice.catClass('Pickles', 'Sphinx');
+        expect(cat.getName()).toBe('Pickles');
+        expect(cat.getBreed()).toBe('Sphinx');
+    })
 })
 
 
--- src/ts-practice.ts
+++ src/ts-practice.ts
@@ -8,58 +8,81 @@
 //     return(`${greeting}! I have ${catNum} cats! ${story}. Was it a vibe? Literally ${vibez}`);
 // }
 
+
 // SIMPLE TYPES
-const myString: string = 'Hello World';
-const myNumber: number = 24;
-const myBoolean: boolean = true;
+export const myString: string = 'Hello World';
+export const myNumber: number = 24;
+export const myBoolean: boolean = true;
 
 // SPECIAL TYPES
-const myAnyType: any = 'I went to yoga';
-const myUndefined:undefined = undefined;
-const myNull: null = null;
+export const myAnyType:any = 'I went to yoga';
+export const myUndefined:undefined = undefined;
+export const myNull: null = null;
 
 // ARRAYS
-const myStringArr: string[] = ['Romad', 'Smoke', 'Fire', 'Nacho', 'The Detective']
-const myNumArr: number[] = [24, 850, 350, 851];
+export const myStringArr: string[] = ['Romad', 'Smoke', 'Fire', 'Nacho', 'The Detective']
+export const myNumArr: number[] = [24, 850, 350, 851];
 
 // TUPLES
-const myTuple: [number, string, boolean] = [24, 'cats', true];
+export const myTuple: [number, string, boolean] = [24, 'cats', true];
 
 // OBJECT TYPES
-const myPet: {name: string, weight:number} = {
+export const myPet: {name: string, weight:number} = {
     name: 'Romad',
     weight: 20
 }
 
 //ENUM TYPES
-enum myCatEnum {
+export enum myCatEnum {
     Romad,
     Smoke,
     Fire,
 }
 
 // ALIASES AND INTERFACES
-type catAge = number;
-const theDetectiveAge: catAge = 5;
+export type catAge = number;
+export const theDetectiveAge: catAge = 5;
 
-interface ICat {
+export interface ICat {
     name: string,
     weight: number,
 }
 
-const cat: ICat= {
+export const cat: ICat= {
     name: 'Fire',
     weight: 12
 };
 
 // UNION TYPES
-const myUnionType: string | number = 'hello world';
+export const myUnionType: string | number = 'hello world';
 
 // FUNCTIONS 
-function addCats(num1: number, num2:number): string {
+export function getCatNumber(): number {
+    //I own one cat
+    return 1;
+}
+
+export function myVoidCatFunction():void {
+    console.log('This cat function returns void.');
+}
+
+export function addCats(num1: number, num2:number): string {
     return `I have ${num1 + num2} cats!`;
 }
 
+// CLASSES
+export class catClass{
+    public constructor(private name:string, private breed?: string){};
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getBreed(): string | undefined{
+        return this.breed;
+    }
+
+}
 
 const story:string = 'I went to yoga'
 const vibez: boolean = true;
@@ -67,6 +90,6 @@ const vibez: boolean = true;
 // const simpleStory = simpleStoryMaker(greeting, catNum, story, vibez);
 // console.log(simpleStory)
 
-export{myString, myNumber, myBoolean, myAnyType, myUndefined, myNull, myStringArr,
-myNumArr, myTuple, myPet, myCatEnum, myUnionType, catAge, theDetectiveAge}
+// export{myNumber, myBoolean, myAnyType, myUndefined, myNull, myStringArr,
+// myNumArr, myTuple, myPet, myCatEnum, myUnionType, catAge, theDetectiveAge, addCats}
 // export {simpleStoryMaker, myString }
